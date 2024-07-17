cffbab87bf9c2e6bb0e78a123b7da1a75382e5c5
hellory4n <hellory4n@gmail.com>
2024-07-16 23:48:53 -0500

    game of thy year

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -9,37 +9,60 @@ public partial class Player : CharacterBody2D {
 	[Export]
 	public double RunningThingy { get; set; } = 1.25;
 	[Export]
+	public AnimatedSprite2D Sprite { get; set; }
+	[Export]
 	public Sprite2D Preview { get; set; }
 	[Export]
 	public RayCast2D PointingRaycast { get; set; }
 	readonly PackedScene lol = GD.Load<PackedScene>("res://blocks/test_block1.tscn");
+	// true = right, false = left
+	// not talking about politics
+	bool lastSide = true;
 
     public override void _PhysicsProcess(double delta)
 	{
 		// movement
-		LookAt(GetGlobalMousePosition());
-		double run = Input.IsActionPressed("run") ? RunningThingy : 1.0;
-		Velocity = Transform.X * Input.GetAxis("move_backwards", "move_forwards") * Speed *
-			new Vector2((float)run, (float)run);
+		float run = Input.IsActionPressed("run") ? (float)RunningThingy : 1.0f;
+		Vector2 dir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        Velocity = dir * Speed * new Vector2(run, run);
 		MoveAndSlide();
 
-		// placing stuff
-		if (Input.IsActionJustPressed("left_click")) {
-			var m = lol.Instantiate<Block>();
-			m.Position = Preview.GlobalPosition;
-			m.Rotation = Rotation;
-			GetParent().AddChild(m);
+		// animations
+		// first figure if it's idle or walk or run
+		string anim = "idle_";
+		if (!dir.IsZeroApprox()) {
+			if (run < 1.1) anim = "walk_";
+			else anim = "run_";
 		}
 
-		// ffuts gnicalp
-		if (Input.IsActionJustPressed("right_click")) {
-			var xd = PointingRaycast.GetCollider();
-			if (xd != null) {
-				// if we checked for physicsbodies we would be able to destroy people
-				if (xd is Block m) {
-					m.QueueFree();
-				}
-			}
-		}
+		// then figure out the direction
+		if (Input.IsActionPressed("move_right")) Sprite.FlipH = false;
+		if (Input.IsActionPressed("move_left")) Sprite.FlipH = true;
+
+		if (Input.IsActionPressed("move_up")) anim += "up";
+		else anim += "side";
+
+		// actually animate :D
+		// we do the check so it doesn't constantly reset
+		if (Sprite.Animation != anim) Sprite.Animation = anim;
+
+		// // placing stuff
+		// if (Input.IsActionJustPressed("left_click")) {
+		// 	var m = lol.Instantiate<Block>();
+		// 	m.Position = Preview.GlobalPosition;
+		// 	m.Rotation = Rotation;
+		// 	GetParent().AddChild(m);
+		// }
+
+		// // ffuts gnicalp
+		// if (Input.IsActionJustPressed("right_click")) {
+		// 	var xd = PointingRaycast.GetCollider();
+		// 	if (xd != null) {
+		// 		// if we checked for physicsbodies we would be able to destroy people
+		// 		if (xd is Block m) {
+		// 			m.QueueFree();
+		// 		}
+		// 	}
+		// }
 	}
 }
