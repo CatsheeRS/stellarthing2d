a0ab99f5ab6aaf85040834674113bbb3cc42a42a
hellory4n <hellory4n@gmail.com>
2024-07-20 20:02:53 -0500

    help

---
+++ src/blocks/GetBlockStoreThing.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace stellarthing;
+
+public static class GetBlockStoreThing {
+    public static Dictionary<string, Item> Fuckery { get; set; } = new() {
+        {"chair", new Item {
+            
+        }}
+    };
+}
---
+++ src/boring/saving/Inventory.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Godot;
+
+namespace stellarthing;
+
+public class Inventory : IConfigData {
+    public string GetFilename() => "%universe/inventory.json";
+    public Dictionary<string, Item> Items { get; set; } = [];
+}
+
+public class Item {
+    public string Scene { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string Image { get; set; } = "";
+    public uint Amount { get; set; } = 0;
+    public int Oxygen { get; set; } = 0;
+    public int Water { get; set; } = 0;
+    public int RocketFuel { get; set; } = 0;
+    public int Uranium { get; set; } = 0;
+    public int Gunpowder { get; set; } = 0;
+    public int Silicon { get; set; } = 0;
+    public int Iron { get; set; } = 0;
+    public int Steel { get; set; } = 0;
+    public int Titanium { get; set; } = 0;
+    public int Diamond { get; set; } = 0;
+    public int Ruby { get; set; } = 0;
+    public int Emerald { get; set; } = 0;
+    public int Sapphire { get; set; } = 0;
+    public int Obsidian { get; set; } = 0;
+}
