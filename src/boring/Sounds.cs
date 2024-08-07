73e6500975aad599d011f89612d7db27e6162740
hellory4n <hellory4n@gmail.com>
2024-08-06 23:24:44 -0500

    v0.7 and stuff

---
+++ src/boring/Sounds.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+namespace stellarthing;
+
+public partial class Sounds : Node {
+}
+
+public enum SoundEnum {
+    Button,
+    Walk,
+    Build,
+    Place,
+    Remove,
+    Engine,
+    Fan,
+    Damage,
+    Die,
+    Alarm,
+}
