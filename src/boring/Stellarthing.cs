0afd816830a725ddd8b74d9d9d9fde1e2d6fc7b6
hellory4n <hellory4n@gmail.com>
2024-08-08 14:39:21 -0500

    help

--- src/boring/Stellarthing.cs
+++ src/boring/Stellarthing.cs
@@ -11,4 +11,24 @@ public partial class Stellarthing : Node {
 	/// name of the current universe, used for saving
 	/// </summary>
 	public static string CurrentUniverse { get; set; } = "";
+	AudioStreamPlayer player = new() {
+		Stream = GD.Load<AudioStream>("res://assets/sounds/one_synth_note.mp3")
+	};
+
+    public override void _Ready()
+    {
+        AddChild(player);
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("click")) {
+			var lol = GetViewport().GuiGetFocusOwner();
+			if (lol != null) {
+				if (lol is BaseButton h) {
+					player.Play();
+				}
+			}
+		}
+    }
 }
