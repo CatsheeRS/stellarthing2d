aa0583fedf246140f48d8e69faa489b536e1db06
hellory4n <hellory4n@gmail.com>
2024-08-11 22:42:26 -0500

    i giveu p gonna rewrite the entire thing

--- src/boring/saving/Resources.cs
+++ src/boring/saving/Resources.cs
@@ -1,22 +1,12 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace stellarthing;
 
 public static class Resources {
-    public static int Oxygen { get; set; } = 0;
-    public static int Water { get; set; } = 0;
-    public static int RocketFuel { get; set; } = 0;
-    public static int Uranium { get; set; } = 0;
-    public static int Gunpowder { get; set; } = 0;
-    public static int Silicon { get; set; } = 0;
-    public static int Iron { get; set; } = 0;
-    public static int Steel { get; set; } = 0;
-    public static int Titanium { get; set; } = 0;
-    public static int Diamond { get; set; } = 0;
-    public static int Ruby { get; set; } = 0;
-    public static int Emerald { get; set; } = 0;
-    public static int Sapphire { get; set; } = 0;
-    public static int Obsidian { get; set; } = 0;
+    public static Dictionary<string, int> Stuff { get; set; } = [
+        ""
+    ];
 
     public class SaveVersion {
         public int Oxygen { get; set; } = 0;
@@ -34,21 +24,21 @@ public static class Resources {
         public int Sapphire { get; set; } = 0;
         public int Obsidian { get; set; } = 0;
     }
+}
 
-    public static SaveVersion ToSaveVersion()
-    {
-        return new SaveVersion {
-            Oxygen = Oxygen,
-            Water = Water,
-            RocketFuel = RocketFuel,
-            Uranium = Uranium,
-            Gunpowder = Gunpowder,
-            Silicon = Silicon,
-            Iron = Iron,
-            Steel = Steel,
-            Titanium = Titanium,
-            Diamond = Diamond,
-            Ruby =
-        };
-    }
+public enum ResourceTypes {
+    Oxygen,
+    Water,
+    RocketFuel,
+    Uranium,
+    Gunpowder,
+    Silicon,
+    Iron,
+    Steel,
+    Titanium,
+    Diamond,
+    Ruby,
+    Emerald,
+    Sapphire,
+    Obsidian,
 }
