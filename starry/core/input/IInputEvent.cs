47e8b1f2503bdf6bbb1e52bbdda499a6ae82f54b
hellory4n <hellory4n@gmail.com>
2024-09-03 20:02:17 -0500

    el mierda

--- starry/core/input/IInputEvent.cs
+++ starry/core/input/IInputEvent.cs
@@ -2,7 +2,33 @@ using static starry.Starry;
 
 namespace starry;
 
+// TODO: get a controller and add support for that
+
 /// <summary>
 /// base of all input events
 /// </summary>
-public interface IInputEvent {}
+public interface IInputEvent {
+    public InputType getType();
+}
+
+/// <summary>
+/// used for casting input events
+/// </summary>
+public enum InputType {
+    /// <summary>
+    /// a key was pressed
+    /// </summary>
+    keypress,
+    /// <summary>
+    /// the mouse's position was updated
+    /// </summary>
+    mouse_motion,
+    /// <summary>
+    /// a mouse button was pressed
+    /// </summary>
+    mouse_button,
+    /// <summary>
+    /// the user has scrolled
+    /// </summary>
+    mouse_scroll
+}
