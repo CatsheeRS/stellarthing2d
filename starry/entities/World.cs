85bc0f45db92f9751ffd2fd9b59f86f824da3a75
hellory4n <hellory4n@gmail.com>
2024-09-10 22:19:08 -0500

    el câmera

--- starry/entities/World.cs
+++ starry/entities/World.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using static starry.Starry;
 
 namespace starry;
@@ -115,6 +116,51 @@ public static class World {
             foreach (var entity in getGroup("layers.game_world")) {
                 if (func(entity)) return;
             }
+        }
+    }
+
+    /// <summary>
+    /// the position of the 3d camera
+    /// </summary>
+    public static vec3 cameraPosition {
+        get => vec3(Renderer.camera3d.Position.X, Renderer.camera3d.Position.Y, Renderer.camera3d.Position.Z);
+        set => Renderer.camera3d.Position = new Vector3((float)value.x, (float)value.y, (float)value.z);
+    }
+
+    /// <summary>
+    /// the position the camera is pointing towards
+    /// </summary>
+    public static vec3 cameraTarget {
+        get => vec3(Renderer.camera3d.Target.X, Renderer.camera3d.Target.Y, Renderer.camera3d.Target.Z);
+        set => Renderer.camera3d.Target = new Vector3((float)value.x, (float)value.y, (float)value.z);
+    }
+    
+    /// <summary>
+    /// true will switch to perspective, false will switch to orthographic. the difference is that in a perspective projection, things get smaller as they get further away, which is how we naturally see. meanwhile, an orthographic projection doesn't, which is great for isometric cameras and rendering 2d if you want to do that in 3d for some reason
+    /// </summary>
+    public static bool cameraPerspective {
+        get => Renderer.camera3d.Projection == Raylib_cs.CameraProjection.Perspective;
+        set => Renderer.camera3d.Projection = value ? Raylib_cs.CameraProjection.Perspective :
+            Raylib_cs.CameraProjection.Orthographic;
+    }
+
+    /// <summary>
+    /// the field-of-view of the camera, from 30 to 150, default is 90
+    /// </summary>
+    public static double cameraFov {
+        get {
+            float elmierda = Renderer.camera3d.FovY;
+            float RADICAL = elmierda * (MathF.PI / 180f);
+            // glad that twitter was banned in brazil
+            float RATIO = settings.renderSize.x / settings.renderSize.y;
+            float HADICAL = 2f * MathF.Atan(MathF.Tan(RADICAL / 2f) * RATIO);
+            return HADICAL * (180f / MathF.PI);
+        }
+        set {
+            float hfov = (float)(value * (Math.PI / 180));
+            // glad that twitter was banned in brazil
+            float RATIO = settings.renderSize.x / settings.renderSize.y;
+            Renderer.camera3d.FovY = 2f * MathF.Atan(MathF.Tan(hfov / 2f) * RATIO) * (180f / MathF.PI);
         }
     }
 }
