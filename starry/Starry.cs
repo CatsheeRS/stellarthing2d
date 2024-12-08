7aa981746bacd59ecdd336a65f741cf2f13d225e
hellory4n <hellory4n@gmail.com>
2024-12-08 12:03:54 -0500

    adios

--- starry/Starry.cs
+++ starry/Starry.cs
@@ -46,17 +46,10 @@ public static class Starry {
 
         settings.startup();
         
-        Audio reality = await load<Audio>("reality.mp3");
-        //method 1 (better)
-        AudioManager audManager = new();
-        audManager.play(reality);
-        audManager.setVolume(10);
-        
         Sprite stellarballs = await load<Sprite>("stellarthing.png");
         Sprite crapbg = await load<Sprite>("restest.png");
         Font font = await load<Font>("font/pixel-unicode.ttf");
         double rot = 0;
-        float angle = 0;
         while (!await Window.isClosing()) {
             Graphics.clear(color.black);
             Graphics.drawSprite(crapbg, (0, 0, 320, 180), (0, 0), 0, (255, 255, 255, 127));
@@ -64,20 +57,8 @@ public static class Starry {
             Graphics.drawSprite(stellarballs, (320, 0, 78, 16), (0, 0), 90, color.red);
             Graphics.drawSprite(stellarballs, (0, 180 - 32, 32, 32), (0.5, 0.5), 15, color.green);
             Graphics.drawSprite(stellarballs, (320 - 78, 180 - 16, 78, 16), (0.5, 0.5), rot, color.blue);
-            //Graphics.drawText("¡Hola! ¿Cómo estás?", font, (50, 50), (255, 0, 255, 255));
-            Graphics.drawText(angle.ToString(), font, (50, 50), (255, 0, 255, 255));
+            Graphics.drawText("¡Hola! ¿Cómo estás?", font, (50, 50), (255, 0, 255, 255));
             //Graphics.drawTextWordwrap("Промышленная революция и ее последствия стали катастрофой для человечества.", font, (66, 66, 100, 100), color.skyBlue);
-            
-            angle += 1;
-            if (angle >= 360) angle = 0;
-
-            float l = MathF.Sin((float)Math.deg2rad(angle));
-            float r = MathF.Cos((float)Math.deg2rad(angle));
-            
-            l = (l + 1) / 2;
-            r = (r + 1) / 2;
-            
-            audManager.setPosition((l, r), vec2.zero);
             
             // stuff
             await DebugMode.update();
