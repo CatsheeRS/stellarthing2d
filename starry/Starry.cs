acc013a5aae93c1fd9116eebcd055e10ac9ee031
hellory4n <hellory4n@gmail.com>
2024-12-07 21:24:19 -0500

    fuck

--- starry/Starry.cs
+++ starry/Starry.cs
@@ -57,7 +57,7 @@ public static class Starry {
             Graphics.drawSprite(stellarballs, (0, 180 - 32, 32, 32), (0.5, 0.5), 15, color.green);
             Graphics.drawSprite(stellarballs, (320 - 78, 180 - 16, 78, 16), (0.5, 0.5), rot, color.blue);
             Graphics.drawText("¡Hola! ¿Cómo estás?", font, (50, 50), (255, 0, 255, 255));
-            Graphics.drawText("Промышленная революция и ее последствия стали катастрофой для человечества.", font, (50, 100), (255, 0, 255, 255));
+            Graphics.drawTextWordwrap("Промышленная революция и ее последствия стали катастрофой для человечества.", font, (0, 66, 100, 100), color.skyBlue);
             rot += 5;
             Graphics.endDrawing();
         }
